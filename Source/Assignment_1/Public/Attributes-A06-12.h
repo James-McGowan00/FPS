ba4e60ad42a13da5fa5c5eb3efb0b88310c06ff1
James McGowan <43676496+m012343i@users.noreply.github.com>
2019-12-05 09:07:44 +0000

    Revert "Revert "nothing""
    This reverts commit a0f9b5179d0df4756f72411bd3b97a43893988f8.

---
+++ Source/Assignment_1/Public/Attributes-A06-12.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Attributes.generated.h"
+
+class AAssignment_1HUD;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ASSIGNMENT_1_API UAttributes : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAttributes();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+
+	int GetMaxHealth() { return _MaxHealth; }
+	int GetCurrentHealth() { return _CurrentHealth; }
+	int GetMaxStamina() { return _MaxStamina; }
+	int GetCurrentStamina() { return _CurrentStamina; }
+
+	UFUNCTION()
+	void TakeDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+	UFUNCTION(BlueprintCallable)
+		void RecoverHealth();
+
+	UPROPERTY(BlueprintReadOnly)
+		float _CurrentHealth;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Stats")
+		float _MaxHealth;
+
+
+	UPROPERTY(EditAnywhere, Category = "Stats")
+		int _MaxStamina;
+
+	UPROPERTY(VisibleInstanceOnly, Category = "Stats")
+		int _CurrentStamina;
+
+	UPROPERTY(EditAnywhere, Category = "Stats")
+		float _StaminaRecoverRate;
+
+	UPROPERTY(EditAnywhere, Category = "Stats")
+		float _StaminaRecoverDelay;
+	float _StaminaRecoverDeplayTimer;
+
+	void RecoverStanima(float DeltaTime);
+
+protected:
+	UPROPERTY()
+		AAssignment_1HUD* _Hud;
+};
